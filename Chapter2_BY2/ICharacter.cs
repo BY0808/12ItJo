0de6507e4d43eb81c9a628c9f5d43774c4b24f41
lsy1304 <lsy130425@gmail.com>
2024-04-30 14:09:14 +0900

    [Add] ICharacter.cs

---
+++ Chapter2_BY2/ICharacter.cs
@@ -0,0 +1,13 @@
+﻿namespace Chapter2_BY2
+{
+    internal interface ICharacter
+    {
+        int Hp { get; set; }
+        int Level { get; }
+        string Name { get; }
+        int Attack { get; }
+        bool IsDead { get; }
+        void TakeDamage(int damage);
+
+    }
+}
