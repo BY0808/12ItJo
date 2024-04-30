ccd567274877456d35749fc4f8d6bf1d502be73b
lsy1304 <lsy130425@gmail.com>
2024-04-30 19:50:23 +0900

    [Commit] Item.cs

--- Chapter2_BY2/Item.cs
+++ Chapter2_BY2/Item.cs
@@ -11,22 +11,22 @@ namespace Chapter2_BY2
 
     internal class Item
     {
-        public string Name { get; }
-        public string Desc { get; }
-        private ItemType type;
-        public int Atk { get; }
-        public int Def { get; }
-        public int Hp { get; }
-        public int Price { get; }
-        public bool isEquipped { get; private set; }
-        public bool isPurchased { get; private set; }
+        public string Name { get; } // 이름
+        public string Desc { get; } // 설명
+        public ItemType Type { get; } // 아이템 타입
+        public int Atk { get; } // 공격력
+        public int Def { get; } // 방어력
+        public int Hp { get; } // 체력
+        public int Price { get; } // 가격
+        public bool isEquipped { get; private set; } // 장착 여부
+        public bool isPurchased { get; private set; } // 구매 여부
 
         //isEquipped 와 is Purchased 를 기본적으로 false로 셋팅
         public Item(string name, string desc, ItemType type, int atk, int def, int hp, int price, bool isEquipped = false, bool isPurchased = false)
         {
             Name = name;
             Desc = desc;
-            this.type = type;
+            Type = type;
             Atk = atk;
             Def = def;
             Hp = hp;
@@ -38,10 +38,10 @@ namespace Chapter2_BY2
         //아이템 정보 보여줄때 타입이 비슷한것 2가지
         //1.인벤토리에서 그냥 내가 갖고있는 아이템 보여줄 때
         //2. 장착관리에서 내가 어떤 아이템을 낄지 말지 결정할 때
-        internal void PrintItemStatDescription(bool withNumber = false, int idx = 0)
+        internal void PrintItemStatDescription(bool withNumber = false, int idx = 0) // 인벤토리에서 아이템을 표시하는 메서드
         {
             Console.Write("- ");
-            if (withNumber) //withNumber 가 true 라면
+            if (withNumber) //withNumber 가 true 라면 (숫자랑 같이 출력)
             {
                 Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.Write($"{idx} ");
@@ -72,7 +72,7 @@ namespace Chapter2_BY2
 
         }
 
-        public void PrintStoreItemDescription(bool withNumber = false, int idx = 0)
+        public void PrintStoreItemDescription(bool withNumber = false, int idx = 0) // 상점에서 아이템을 표시하는 메서드
         {
             Console.Write("- ");
             if (withNumber) //withNumber 가 true 라면
@@ -107,12 +107,12 @@ namespace Chapter2_BY2
 
         }
 
-        internal void ToggleEquipStatus()
+        internal void ToggleEquipStatus() // 장착 여부 반전시키는 메서드
         {
             isEquipped = !isEquipped;
         }
 
-        internal void Purchase()
+        internal void Purchase() // 구매 여부 확정시키는 메서드
         {
             isPurchased = !isPurchased;
         }
