8f52bf6340373d00edbc61c5d342d44d4a3822dc
lsy1304 <lsy130425@gmail.com>
2024-05-02 13:56:18 +0900

    [Feature] Monstre.cs
    미니언, 고블린, 드래곤 레벨 설정 변경
    미니언 1~3
    고블린 2~4
    드래곤 3~4

--- Chapter2_BY2/Monster.cs
+++ Chapter2_BY2/Monster.cs
@@ -47,9 +47,9 @@ namespace Chapter2_BY2
         /// </summary>>
         public bool IsDead => Hp <= 0; // 체력이 0 이하인 경우 true
 
-        public Monster(string name, int hpMulti, int attackMulti)
+        public Monster(int level, string name, int hpMulti, int attackMulti)
         {
-            Level = new Random().Next(1, 6); // 레벨을 1 ~ 5 사이에서 랜덤
+            Level = level; // 레벨을 1 ~ 5 사이에서 랜덤
             Name = name;
             Hp = Level * hpMulti; // 체력 = 레벨 * 체력 배수
             Atk = Level * attackMulti; // 공격력 = 레벨 * 공격력 배수
@@ -70,7 +70,7 @@ namespace Chapter2_BY2
     /// </summary>
     public class Minion : Monster
     {
-        public Minion() : base("미니언", 5, 3) { } // 미니언 생성자
+        public Minion() : base(new Random().Next(1, 4), "미니언", 5, 3) { } // 미니언 생성자 (레벨 1~3)
     }
 
     /// <summary>
@@ -78,12 +78,12 @@ namespace Chapter2_BY2
     /// </summary>
     public class Goblin : Monster
     {
-        public Goblin() : base("고블린", 7, 4) { } // 고블린 생성자
+        public Goblin() : base(new Random().Next(2, 5), "고블린", 7, 4) { } // 고블린 생성자 (레벨 2~4)
     }
 
     //드래곤 클래스
     public class Dragon : Monster
     {
-        public Dragon() : base("드래곤", 8, 5) { } // 드래곤 생성자
+        public Dragon() : base(new Random().Next(3, 5), "드래곤", 8, 5) { } // 드래곤 생성자 (레벨 3~4)
     }
 }
